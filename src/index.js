61daf2c403254459e1f765c38ef3ce56b8e2edec
yato03 <miguelhs3523@gmail.com>
2022-07-05 16:23:20 +0200

    configuracion heroku

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ app.use((req, res, next) => {
   res.status(404).json({ error: 'Not found' })
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT)
 })
